0af791fcf338ede1822f20dac050f1bd6d1aa505
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2023-10-16 22:55:32 +0200

    plugins: upgrade python support

--- lua/plugins/extras/lang/python.lua
+++ lua/plugins/extras/lang/python.lua
@@ -1,9 +1,22 @@
 return {
   {
+    "vsedov/py.nvim",
+    ft = { "python" },
+    config = function()
+      require("py").setup()
+    end,
+  },
+  {
     "linux-cultist/venv-selector.nvim",
     cmd = "VenvSelect",
     opts = {
-      name = { "venv", ".venv" },
+      name = {
+        "venv",
+        ".venv",
+        "env",
+        ".env",
+      },
+      dap_enabled = true,
     },
     keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
   },
