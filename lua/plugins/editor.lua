9b7cb027c7b6811a54c52cda8a2da53b73bf3edd
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2023-10-16 23:07:34 +0200

    plugins: configure neo-tree hidden files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -9,6 +9,16 @@ return {
           enabled = true,
         },
         group_empty_dirs = true,
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".git",
+          },
+          never_show = {},
+        },
       },
     },
   },
