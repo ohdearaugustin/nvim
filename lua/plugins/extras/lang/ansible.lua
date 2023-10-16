6ab101082a8df821ceb1b35de7bbb187601a936e
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2023-10-16 22:54:40 +0200

    plugins: add ansible support

---
+++ lua/plugins/extras/lang/ansible.lua
@@ -0,0 +1,11 @@
+return {
+  -- correctly setup mason lsp extensions
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ansible-language-server" })
+      end
+    end,
+  },
+}
