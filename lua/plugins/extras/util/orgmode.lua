ab10035af82b263ed25b60495b1e8ba5dae459f5
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2024-05-16 01:32:21 +0200

    fix orgmode

--- lua/plugins/extras/util/orgmode.lua
+++ lua/plugins/extras/util/orgmode.lua
@@ -1,12 +1,12 @@
-{
-  'nvim-orgmode/orgmode',
-  event = 'VeryLazy',
-  ft = { 'org' },
+return {
+  "nvim-orgmode/orgmode",
+  event = "VeryLazy",
+  ft = { "org" },
   config = function()
     -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+    require("orgmode").setup({
+      org_agenda_files = "~/orgfiles/**/*",
+      org_default_notes_file = "~/orgfiles/refile.org",
     })
 
     -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
