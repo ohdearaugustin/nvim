4e3174a209319da4c475c2b702fdfba844cf9d47
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2024-05-30 00:58:38 +0200

    remap neotree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,47 @@
 -- Add any additional keymaps here
 local wk = require("which-key")
 
+-- Key mapping to switch between buffer and Neotree
+-- Function to toggle between Neotree and the last used buffer
+function ToggleNeoTree()
+  local current_win = vim.api.nvim_get_current_win()
+  local neotree_win = nil
+
+  -- Find the Neotree window
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+    if bufname:match("neo%-tree filesystem") then
+      neotree_win = win
+      break
+    end
+  end
+
+  if neotree_win then
+    if current_win == neotree_win then
+      -- If currently in Neotree, switch to the previous buffer
+      vim.cmd("wincmd p")
+    else
+      -- Otherwise, switch to the Neotree window
+      vim.api.nvim_set_current_win(neotree_win)
+    end
+  else
+    -- If Neotree is not open, open it
+    vim.cmd("Neotree toggle")
+  end
+end
+vim.keymap.del("n", "<leader>E")
+vim.keymap.del("n", "<leader>e")
+vim.keymap.set("n", "<leader>e", ":lua ToggleNeoTree()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>E", function()
+  vim.cmd("Neotree toggle")
+end, { noremap = true, silent = true })
+local neotree_mappings = {
+  e = { "Toggle Neotree" },
+  E = { "Explorer Neotree" },
+}
+
+wk.register(neotree_mappings, { prefix = "<leader>" })
+
 local helm_mappings = {
   k = {
     name = "Kubernetes",
