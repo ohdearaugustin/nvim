13e632c3196c7da4b77a00f75338c1e90e56d3b7
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2024-06-04 01:32:04 +0200

    kube_utils: change to local plugin

--- lua/plugins/extras/util/kube_utils.lua
+++ lua/plugins/extras/util/kube_utils.lua
@@ -1,10 +1,6 @@
 return {
   {
-    "h4ckm1n-dev/kube-utils-nvim",
-    -- dir = "/local/path/to/plugin",
-    branch = "main", -- comment to use local folder
-    config = function()
-      require("init").setup()
-    end,
+    dir = "/home/augustin/programming/kube-utils-nvim",
+    opts = {},
   },
 }
