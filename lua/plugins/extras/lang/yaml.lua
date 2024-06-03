64d9dd694e9d8641c1fa89b1ac400f3d4e9069f0
ohdearaugustin <ohdearaugustin@users.noreply.github.com>
2024-06-04 01:33:22 +0200

    yamlls: add config

--- lua/plugins/extras/lang/yaml.lua
+++ lua/plugins/extras/lang/yaml.lua
@@ -2,11 +2,20 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      settings = {
-        yaml = {
-          format = {
-            enable = false,
-            bracketSpacing = false,
+      yamlls = {
+        settings = {
+          yaml = {
+            format = {
+              enable = false,
+              bracketSpacing = false,
+            },
+            schemaStore = {
+              enable = true,
+              url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+            schemas = {
+              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            },
           },
         },
       },
